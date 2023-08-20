a2f5b568042fdcd39540254e056458f97a8f0c59
aenon0 <95585844+aenon0@users.noreply.github.com>
2023-08-20 21:54:03 +0300

    ..

---
+++ HRLeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRLeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequest { get; set; }
+        public LeaveManagementDbContext(DbContextOptions options) : base(options)   
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;    
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
---
+++ HRLeaveManagement.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HRLeaveManagement.Application.Persistance.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRLeaveManagement.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+        public GenericRepository(LeaveManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<T> Add(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+            return null;
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = Get(id);
+            return entity != null;
+        }
+
+        public async Task<T> Get(int Id)
+        {
+            return await _dbContext.Set<T>().FindAsync(Id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAll()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return null;
+        }
+    }
+}
